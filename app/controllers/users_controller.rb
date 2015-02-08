6906e56c5c7fd6c2381c9f79cab67d3336513cfe
uiruson <uiruson@gmail.com>
2015-02-07 22:34:10 -0800

    Part 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      sessions[:user_id] = @user.id #auto login
+      session[:user_id] = @user.id #auto login
       redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
   resources :movies
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
